c2997564891d715fee0bcbb6c170d4b5edbda6bb
Muhammad Imran Hussain Khan <isoft_isoft@yahoo.com>
2013-09-15 09:00:00 +0000

    Bootstrap CRM foundation

--- script/app/Services/Crm/ProjectTimelineService.php
+++ script/app/Services/Crm/ProjectTimelineService.php
@@ -7,9 +7,9 @@ class ProjectTimelineService
     public function milestones(): array
     {
         return [
-            ['year' => '2013', 'focus' => 'Foundation, lead tracking, and sales pipeline'],
-            ['year' => '2014', 'focus' => 'Billing automation, reporting dashboards, and collaboration workflows'],
-            ['year' => '2015', 'focus' => 'Client portal, activity timelines, and customer success tools'],
+            ['year' => '2013', 'focus' => 'Foundation, sales pipeline, and account planning'],
+            ['year' => '2014', 'focus' => 'Billing automation, reporting dashboards, and team collaboration'],
+            ['year' => '2015', 'focus' => 'Client portal, notifications, and activity timeline'],
         ];
     }
 }
--- script/docs/crm-roadmap.md
+++ script/docs/crm-roadmap.md
@@ -1,10 +1,5 @@
 ﻿# CRM Project Timeline
 
-- 2013: foundation, lead pipeline, and initial sales workflows.
-- 2014: invoicing automation, reporting dashboards, and team collaboration.
+- 2013: foundation, lead lifecycle, and initial sales workflows.
+- 2014: billing automation, reporting dashboards, and team collaboration.
 - 2015: client portal, notifications, and activity timeline improvements.
-- 2013: sales pipeline and lead lifecycle tracking.
-- 2014: invoicing automation and recurring billing preparation.
-- 2014: reporting dashboards and revenue forecasts.
-- 2014: team collaboration and notification channels.
-- 2015: client portal and activity timeline.
