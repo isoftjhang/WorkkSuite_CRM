9dd78017113a421a1c3475e4b8a2452c1b611d1b
Muhammad Imran Hussain Khan <isoft_isoft@yahoo.com>
2013-11-18 09:00:00 +0000

    Implement sales pipeline workflows

--- script/app/Services/Crm/LeadPipelineService.php
+++ script/app/Services/Crm/LeadPipelineService.php
@@ -6,6 +6,12 @@ class LeadPipelineService
 {
     public function stages(): array
     {
-        return ['Discovery', 'Qualified', 'Proposal', 'Negotiation', 'Closed'];
+        return [
+            ['name' => 'Discovery', 'owner' => 'Sales'],
+            ['name' => 'Qualified', 'owner' => 'Account Executive'],
+            ['name' => 'Proposal', 'owner' => 'Sales'],
+            ['name' => 'Negotiation', 'owner' => 'Account Executive'],
+            ['name' => 'Closed', 'owner' => 'Sales Ops'],
+        ];
     }
 }
--- script/docs/crm-roadmap.md
+++ script/docs/crm-roadmap.md
@@ -3,3 +3,4 @@
 - 2013: foundation, lead pipeline, and initial sales workflows.
 - 2014: invoicing automation, reporting dashboards, and team collaboration.
 - 2015: client portal, notifications, and activity timeline improvements.
+- 2013: sales pipeline and lead lifecycle tracking.
